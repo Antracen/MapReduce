809dfb3ada19f46570a021edecd5ae6d2e656b07
Martin Wass <wass@W530.wass>
2019-05-31 19:35:13 +0200

    hard code is god

---
+++ bin/sorme_benchmarkers/test_starter.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+weak=true
+strong=true
+
+if [ "$weak" = true ] ; then
+    for filename in /cfs/nobackup/s/sorme/MapReduce/bin/Benchmarkers/Weak/*
+    do
+        sbatch ./${filename}
+    done
+fi
+
+if [ "$strong" = true ] ; then
+    for filename in /cfs/nobackup/s/sorme/MapReduce/bin/Benchmarkers/Strong/*
+    do
+        sbatch ./${filename}
+    done
+fi
