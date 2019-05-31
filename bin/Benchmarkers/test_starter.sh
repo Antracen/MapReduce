17535e1d8f3e760004cd7318a6bcf452290ecc5c
Martin Wass <wass@W530.wass>
2019-05-31 19:47:32 +0200

    oops

--- bin/Benchmarkers/test_starter.sh
+++ bin/Benchmarkers/test_starter.sh
@@ -6,13 +6,13 @@ strong=true
 if [ "$weak" = true ] ; then
     for filename in /cfs/nobackup/m/mawass/MapReduce/bin/Benchmarkers/Weak/*
     do
-        sbatch ./${filename}
+        sbatch ${filename}
     done
 fi
 
 if [ "$strong" = true ] ; then
     for filename in /cfs/nobackup/m/mawass/MapReduce/bin/Benchmarkers/Strong/*
     do
-        sbatch ./${filename}
+        sbatch ${filename}
     done
 fi
--- bin/sorme_benchmarkers/test_starter.sh
+++ bin/sorme_benchmarkers/test_starter.sh
@@ -6,13 +6,13 @@ strong=true
 if [ "$weak" = true ] ; then
     for filename in /cfs/nobackup/s/sorme/MapReduce/bin/Benchmarkers/Weak/*
     do
-        sbatch ./${filename}
+        sbatch ${filename}
     done
 fi
 
 if [ "$strong" = true ] ; then
     for filename in /cfs/nobackup/s/sorme/MapReduce/bin/Benchmarkers/Strong/*
     do
-        sbatch ./${filename}
+        sbatch ${filename}
     done
 fi
