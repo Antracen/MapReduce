85df9701cdc5d459319abadc0ad7e7c216dc8a47
Martin Wass <wass@W530.wass>
2019-05-31 16:58:59 +0200

    Fix dir

--- bin/Benchmarkers/Weak/40GB_20PROCS.sh
+++ bin/Benchmarkers/Weak/40GB_20PROCS.sh
@@ -8,13 +8,13 @@
 #SBATCH --mail-type=END
 #SBATCH --mail-user=sorme,mawass
 
-mkdir -p /cfs/nobackup/m/mawass/Results
+mkdir -p /cfs/nobackup/m/mawass/MapReduce/Results
 
 file_size=40
 procs=$((file_size / 2))
 
 if=/cfs/klemming/scratch/s/sergiorg/DD2356/input/wikipedia_${file_size}GB.txt
-of=/cfs/nobackup/m/mawass/Results/size_${file_size}GB_procs_${procs}.txt
+of=/cfs/nobackup/m/mawass/MapReduce/Results/size_${file_size}GB_procs_${procs}.txt
 
 export OMP_NUM_THREADS=32
 aprun -q -n $procs /cfs/nobackup/m/mawass/MapReduce/bin/wordcount.out $if $of
