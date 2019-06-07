697bbdc93963c6dc3a9bec5292b40e553bf457fb
Martin Wass <mawass@kth.se>
2019-06-07 11:33:57 +0200

    Optimization flag

--- bin/Benchmarkers/Weak/10GB_5PROCS.sh
+++ bin/Benchmarkers/Weak/10GB_5PROCS.sh
@@ -16,7 +16,7 @@ procs=$file_size
 if=/cfs/klemming/scratch/s/sergiorg/DD2356/input/wikipedia_${file_size}GB.txt
 of=/cfs/nobackup/m/mawass/MapReduce/Results/size_${file_size}GB_procs_${procs}.txt
 
-echo "" > $if
+echo "" > $of
 
 export OMP_NUM_THREADS=32
 aprun -q -n $procs /cfs/nobackup/m/mawass/MapReduce/bin/wordcount.out $if $of
